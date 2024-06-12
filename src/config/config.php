b20f5c969aa8442b27bc280fc20555e0d28bb840
dvyzi <zclombe@edenschool.fr>
2024-06-12 10:24:14 +0200

    resolution bug

--- src/config/config.php
+++ src/config/config.php
@@ -8,13 +8,13 @@ define("VIEWS", '../src/views/');
 // qui permettent de définir l'adresse du serveur, le nom de la base de données et le login
 
 //POUR WINDOWS POUR WINDOWS POUR WINDOWS POUR WINDOWS POUR WINDOWS POUR WINDOWS POUR WINDOWS POUR WINDOWS POUR WINDOWS POUR WINDOWS POUR WINDOWS POUR WINDOWS
-define('HOST', 'localhost:8889');
+define('HOST', 'localhost:3306');
 define('DATABASE', 'cooker');
 define('USER', 'root');
-define('PASSWORD', 'root');
+define('PASSWORD', '');
 
 /* POUR MAC  POUR MAC  POUR MAC  POUR MAC  POUR MAC  POUR MAC  POUR MAC  POUR MAC  POUR MAC  POUR MAC  POUR MAC  POUR
- MAC  POUR MAC  POUR MAC  POUR MAC  POUR MAC
+MAC  POUR MAC  POUR MAC  POUR MAC  POUR MAC
 define('HOST', 'localhost:8889');
 define('DATABASE', 'Cooker');
 define('USER', 'root');
